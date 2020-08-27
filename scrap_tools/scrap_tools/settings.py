520fe47accbbeacfcb3e008c59c994503de47ca1
bnieto <bnieto@quanticotrends.com>
2020-08-27 08:53:02 -0500

    Moved SECRET_KEY

--- scrap_tools/scrap_tools/settings.py
+++ scrap_tools/scrap_tools/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jezdvp7mg)j!5%r3$u6=*wzri3%p&1x%%%0&j^jz-zyrbwilrl'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
