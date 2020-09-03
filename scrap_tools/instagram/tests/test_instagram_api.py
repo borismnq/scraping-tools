92b2b08efb66a20aeeed9e2200ffa774e41600f6
bnieto <bnieto@quanticotrends.com>
2020-09-02 23:36:02 -0500

    Updated instagram unittests

--- scrap_tools/instagram/tests/test_instagram_api.py
+++ scrap_tools/instagram/tests/test_instagram_api.py
@@ -22,15 +22,21 @@ class InstagramTests(TestCase):
 
         self.assertEqual(str(ig_profile), ig_profile.name)
 
-    def test_user_profile_data(self):
-        """Testing that user profile data page exists"""
-        url = "https://www.instagram.com/claroperu/?__a=1"
 
-        headers = {
+class InstagramScrapTests(TestCase):
+
+    def setUp(self):
+        self.url = "https://www.instagram.com/"
+        self.headers = {
             "user-agent": "Mozilla/5.0 (X11; Linux x86_64)" +
             " AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu" +
             " Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36"
         }
+
+    def test_user_profile_data(self):
+        """Testing that user profile data page exists"""
+        url = self.url+"claroperu/?__a=1"
+        headers = self.headers
 
         response = requests.get(url, headers=headers)
         self.assertIsNotNone(response)
